38337df3beea8916a705f89d24e11f77930645eb
Geert Guldentops <geert.guldentops@ipc.be>
2018-09-11 07:31:17 +0200

    Initial commit for the JS version of the Clean Code labs.

---
+++ src/movie/bill.js
@@ -0,0 +1,33 @@
+module.exports = class Bill {
+
+    /**
+     * (1) New customers arrive at your ticket booth and tell you what movie they'd like to see (so keep it in mind!)
+     *
+     * @param runtime   movie's runtime in minutes
+     * @param dayOfWeek day of the week
+     * @param loge      true if seating category is 'loge')
+     * @param threeD    true if the movie's shown in 3D
+     */
+    startPurchase(runtime, dayOfWeek, loge, threeD) {
+
+    }
+
+    /**
+     * (2) Add a ticket to the customers' bill
+     *
+     * @param age     the age of the ticket buyer in years
+     * @param student true if the ticket buyer is a student
+     */
+    addTicket(age, student) {
+
+    }
+
+    /**
+     * (3) Calculate the total admission for the current customer(s)
+     *
+     * @return total in dollars.
+     */
+    finishPurchase() {
+
+    }
+};
---
+++ src/movie/bill.test.js
@@ -0,0 +1,9 @@
+describe('Given an environment', () => {
+
+    describe('When an event happens', () => {
+
+        test('Then something in the system should have changed', () => {
+            expect(true).toBeFalsy();
+        });
+    });
+});
