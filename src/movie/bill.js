f7faa934b4a40e44a8eedbbd77e40e0df823503b
Maxim Rudenko <maxim.rudenko@jidoka.be>
2020-05-27 10:11:43 +0200

    added tests and functionality

--- src/movie/bill.js
+++ src/movie/bill.js
@@ -1,4 +1,4 @@
-module.exports = class Bill {
+class Bill {
 
     /**
      * (1) New customers arrive at your ticket booth and tell you what movie they'd like to see (so keep it in mind!)
@@ -8,7 +8,22 @@ module.exports = class Bill {
      * @param loge      true if seating category is 'loge')
      * @param threeD    true if the movie's shown in 3D
      */
+
+    ticketBasePrices = [];
+    variablePrices =[];
+    extraPrice = 0.0;
+
     startPurchase(runtime, dayOfWeek, loge, threeD) {
+        if (threeD) {
+            this.extraPrice += 3.0;
+        }
+        if(runtime > 120){
+            this.extraPrice += 1.5;
+        }
+        if(loge){
+            this.extraPrice +=2.0;
+        }
+        this.dayOfWeek = dayOfWeek;
 
     }
 
@@ -19,7 +34,35 @@ module.exports = class Bill {
      * @param student true if the ticket buyer is a student
      */
     addTicket(age, student) {
+        let beginPrice = 11.0
+        if (student) {
+            beginPrice = 8.0;
+        }
+        if (age >= 65) {
+            beginPrice = 6.0;
+        }
+        if (age <= 13) {
+            beginPrice = 5.5;
+        }
 
+        switch(this.dayOfWeek) {
+            case "MONDAY":
+            case "TUESDAY":
+            case "WEDNESDAY":
+              break;
+            case "THURSDAY":
+              beginPrice -= 2.0
+              break;
+            case "FRIDAY":
+              break;
+            case "SATURDAY":
+            case "SUNDAY":
+              beginPrice += 1.5
+              break;
+          }
+
+        this.ticketBasePrices.push(beginPrice);
+        this.variablePrices.push(this.extraPrice);
     }
 
     /**
@@ -28,6 +71,11 @@ module.exports = class Bill {
      * @return total in dollars.
      */
     finishPurchase() {
-
+        if(this.ticketBasePrices.length >= 20){
+            this.ticketBasePrices = this.ticketBasePrices.map(x => 6.0);
+        }
+        return this.ticketBasePrices.reduce((a, b) => a + b, 0) + this.variablePrices.reduce((a, b) => a + b, 0);
     }
 };
+
+module.exports = { Bill };
--- src/movie/readme.txt
+++ src/movie/readme.txt
@@ -2,17 +2,17 @@ Movie tickets:
 
 Implement the Bill JS class, the implementation should adhere to the following business rules:
 
-- General Admission: $11.00 per ticket.
-- Students: $8.00 per ticket.
-- Senior Citizen (65 & older): $6.00 per ticket.
-- Children (under 13): $5.50 per ticket.
-- Group Pricing (if 20 people or more): $6.00 per ticket.
+- General Admission: $11.00 per ticket. /
+- Students: $8.00 per ticket. / 
+- Senior Citizen (65 & older): $6.00 per ticket. / 
+- Children (under 13): $5.50 per ticket. /
+- Group Pricing (if 20 people or more): $6.00 per ticket.  /
 
 Exceptions:
 
-- 3D movie: add $3.00 per ticket.
-- Over-length (more than 120 minutes): add $1.50 per ticket.
-- Special Movie Day (Thursday, but does not apply if group-pricing): subtract $2.00 per ticket.
+- 3D movie: add $3.00 per ticket. / 
+- Over-length (more than 120 minutes): add $1.50 per ticket. / 
+- Special Movie Day (Thursday, but does not apply if group-pricing): subtract $2.00 per ticket. /
 - Weekends: add $1.50 per ticket.
 - Balcony Seating: add $2.00 per ticket.
 
