7aeaeea5b44ffc7c31d5345b693676624e41da82
Maxim Rudenko <maxim.rudenko@jidoka.be>
2020-05-27 08:33:31 +0200

    added files

--- src/movie/bill.js
+++ src/movie/bill.js
@@ -1,4 +1,4 @@
-module.exports = class Bill {
+class Bill {
 
     /**
      * (1) New customers arrive at your ticket booth and tell you what movie they'd like to see (so keep it in mind!)
@@ -8,7 +8,39 @@ module.exports = class Bill {
      * @param loge      true if seating category is 'loge')
      * @param threeD    true if the movie's shown in 3D
      */
+
+    ticketPrices = [];
+    extraPrice = 0.0;
+    specialMovieDay = false;
+
     startPurchase(runtime, dayOfWeek, loge, threeD) {
+        if (threeD) {
+            this.extraPrice += 3.0;
+        }
+        if(runtime > 120){
+            this.extraPrice += 1.5;
+        }
+        if(loge){
+            this.extraPrice +=2.0;
+        }
+        switch(dayOfWeek) {
+            case "MONDAY":
+            case "TUESDAY":
+            case "WEDNESDAY":
+              break;
+            case "THURSDAY":
+              this.extraPrice -= 2.0
+              this.specialMovieDay = true;
+              break;
+            case "FRIDAY":
+              break;
+            case "SATURDAY":
+              this.extraPrice += 1.5
+              break;
+            case "SUNDAY":
+              this.extraPrice += 1.5
+              break;
+          }
 
     }
 
@@ -19,7 +51,18 @@ module.exports = class Bill {
      * @param student true if the ticket buyer is a student
      */
     addTicket(age, student) {
+        let beginPrice = 11.0
+        if (student) {
+            beginPrice = 8.0;
+        }
+        if (age >= 65) {
+            beginPrice = 6.0;
+        }
+        if (age <= 13) {
+            beginPrice = 5.5;
+        }
 
+        this.ticketPrices.push(beginPrice + this.extraPrice);
     }
 
     /**
@@ -28,6 +71,11 @@ module.exports = class Bill {
      * @return total in dollars.
      */
     finishPurchase() {
-
+        if(this.ticketPrices.length >= 20){
+            this.ticketPrices = this.ticketPrices.map(x => 6.0);
+        }
+        return this.ticketPrices.reduce((a, b) => a + b, 0);
     }
 };
+
+module.exports = { Bill };
--- src/movie/bill.test.js
+++ src/movie/bill.test.js
@@ -1,9 +1,131 @@
-describe('Given an environment', () => {
+const { Bill } = require('./bill');
 
-    describe('When an event happens', () => {
+let bill;
 
-        test('Then something in the system should have changed', () => {
-            expect(true).toBeFalsy();
-        });
-    });
+beforeEach(() => {
+    bill = new Bill();
 });
+
+test('Then something in the system should have changed', () => {
+    expect(false).toBeFalsy();
+});
+
+test('generalAdmissionShouldBeDefaultPrice', () => {
+    // Arrange
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    bill.addTicket(18, false);
+
+    // Act
+    let billPrice = bill.finishPurchase();
+
+    //Assert
+    expect(billPrice).toBe(11.0);
+});
+
+test('studentAdmissionShouldEqualStudentPrice', () => {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    bill.addTicket(18, true);
+
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(8.0);
+});
+
+
+test('multipleGeneralAdmissionsShouldEqualDefaultPrice', () => {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    bill.addTicket(18, false);
+    bill.addTicket(18, false);
+
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(22.0);
+});
+
+test('_3DShouldAddTobillPrice', () => {
+    bill.startPurchase(100, 'WEDNESDAY', false, true);
+    bill.addTicket(18, false);
+
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(14.0);
+});
+
+test('seniorCitizenOf65ShouldHaveDiscount', () => {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    bill.addTicket(65, false);
+
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(6.0);
+
+});
+
+test('seniorCitizenOver65ShouldHaveDiscount', () => {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    bill.addTicket(80, false);
+
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(6.0);
+
+});
+
+test('childrenUnder13ShouldHaveDiscount', () => {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    bill.addTicket(12,false);
+
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(5.5);
+});
+
+test('groupsOf20ShouldHaveDiscount', ()=> {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    for(let i = 0; i < 20; i++){
+        bill.addTicket(18, false);
+    }
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(120.0);
+});
+
+test('groupsOver20ShouldHaveDiscount', ()=> {
+    bill.startPurchase(100, 'WEDNESDAY', false, false);
+    for(let i = 0; i < 21; i++){
+        bill.addTicket(18, false);
+    }
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(126.0);
+});
+
+test('moviesOver120MinutesShouldBeCharged', ()=> {
+    bill.startPurchase(140, 'WEDNESDAY', false, false);
+    bill.addTicket(18,false);
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(12.5);
+});
+
+test('specialMovieDayShouldHaveDiscount', ()=> {
+    bill.startPurchase(100, 'THURSDAY', false, false);
+    bill.addTicket(18,false);
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(9.0);
+});
+
+test('specialMovieDayDiscountShouldNotApplyOnGroupPricing', ()=> {
+    bill.startPurchase(100, 'THURSDAY', false, false);
+    for(let i = 0; i < 21; i++){
+        bill.addTicket(18, false);
+    }
+    let billPrice = bill.finishPurchase();
+
+    expect(billPrice).toBe(126.0);
+});
+
+
+
+
